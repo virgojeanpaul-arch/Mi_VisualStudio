fbbf974ec090b75c366bb143f81c87cd69ab4045
JeanPaul <virgojeanpaul@gmail.com>
2026-04-05 10:28:42 -0500

    Primer commit

---
+++ Mi_Primera_App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_Primera_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
